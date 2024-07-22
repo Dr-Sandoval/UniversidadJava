8dd90c3bba21279afe6178814a4975ca240cc4c8
Dabir Sandoval <dsandoval3654@gmail.com>
2024-07-22 09:12:37 -0500

    Modificacion de variables y eliminacion de datos innecesarios

--- ReservaHoteles.java
+++ ReservaHoteles.java
@@ -1,32 +1,26 @@
 public class ReservaHoteles {
     public static void main(String[] args) {
 
-        final String NOMBRE_HOTEL = "EL VENEHOTEL";
+        final String NOMBRE_HOTEL = "EL VENE HOTEL";
         System.out.println("NOMBRE_HOTEL = " + NOMBRE_HOTEL);
         System.out.println("");
 
         String nombreCliente = "Carlos Perez";
-        int edad = 35;
-        int numeroHijos = 3;
         int diasEstadia = 35;
         final double TARIFA_DIARIA = 250.99;
         boolean vistaPanoramica = true;
 
         System.out.println("nombreCliente = " + nombreCliente);
-        System.out.println("edad = " + edad);
-        System.out.println("numeroHijos = " + numeroHijos);
         System.out.println("diasEstadia = " + diasEstadia);
         System.out.println("TARIFA_DIARIA = " + TARIFA_DIARIA);
         System.out.println("vistaPanoramica = " + vistaPanoramica);
         System.out.println("");
 
         nombreCliente = "Maria Casanova";
-        edad = 42;
         diasEstadia = 20;
         vistaPanoramica = false;
 
         System.out.println("nombreCliente = " + nombreCliente);
-        System.out.println("edad = " + edad);
         System.out.println("diasEstadia = " + diasEstadia);
         System.out.println("TARIFA_DIARIA = " + TARIFA_DIARIA);
         System.out.println("vistaPanoramica = " + vistaPanoramica);
