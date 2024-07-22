dcbd92ff4bb820bb53da018e8abe7e0341df71fd
Dabir Sandoval <dsandoval3654@gmail.com>
2024-07-22 08:37:33 -0500

    Actualizacion ejercicio Reserva Hoteles

--- ReservaHoteles.java
+++ ReservaHoteles.java
@@ -6,11 +6,13 @@ public class ReservaHoteles {
         System.out.println("");
 
         String nombreCliente = "Carlos Perez";
+        int edad = 35;
         int diasEstadia = 35;
         final double TARIFA_DIARIA = 250.99;
         boolean vistaPanoramica = true;
 
         System.out.println("nombreCliente = " + nombreCliente);
+        System.out.println("edad = " + edad);
         System.out.println("diasEstadia = " + diasEstadia);
         System.out.println("TARIFA_DIARIA = " + TARIFA_DIARIA);
         System.out.println("vistaPanoramica = " + vistaPanoramica);
