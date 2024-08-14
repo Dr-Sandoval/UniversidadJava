f3c2a7bf49da589b6c247edf8537b370608d23b6
Dabir Sandoval <dsandoval3654@gmail.com>
2024-08-14 06:27:07 -0500

    Actualizacion y culminacion de ejercicio

--- EjercicioSebas2.java
+++ EjercicioSebas2.java
@@ -12,40 +12,68 @@ public class EjercicioSebas2 {
         //INVOCACION DE SCANNER 
         Scanner imputUsuario = new Scanner(System.in);
         
-        //CODIGOS
-        int numeroVocales = 0; 
+        //VARIABLES
         String vocalA = "a";
         String vocalE = "e";
         String vocalI = "i";
         String vocalO = "o";
         String vocalU = "u";
+        int letraA = 0;
+        int letraE = 0;
+        int letraI = 0;
+        int letraO = 0;
+        int letraU = 0;
+
+        //CODIGO
 
         System.out.println("\n- - - - - - - EL CONTADOR DE VOCALES - - - - - - - \n");
 
-        System.out.println("Ingresa una palabra aleatoria: ");
-        String palabraRandom = "casa";
+        System.out.print("Ingresa una palabra aleatoria: ");
+        String palabraRandom = imputUsuario.nextLine().toLowerCase();
         int longitudPalabra = palabraRandom.length();
-//        System.out.println("La primera letra es: " + palabraRandom.charAt(0));
-//        System.out.println("La primera letra es: " + vocalA.charAt(0));
 
+        System.out.println("La palabra aleatoria que elegiste fue = " + palabraRandom);
+        System.out.println("La cantidad de letras de la palabra aleatoria es de: " + longitudPalabra);
 
-        System.out.println("palabraRandom = " + palabraRandom);
-        System.out.println("La cantidad de letras de la palabra random es de: " + longitudPalabra);
+        System.out.println("\n*********************************************************************************\n");
 
         for (int i = 0; i < longitudPalabra; i++) {
 
+
             if (palabraRandom.charAt(i) == vocalA.charAt(0)){
 
-                numeroVocales += i;
+                letraA += 1;
 
-            }else{
-                System.out.println("nada");
+            }else if ((palabraRandom.charAt(i) == vocalE.charAt(0))){
+
+                letraE += 1;
+
+            } else if (palabraRandom.charAt(i) == vocalI.charAt(0)){
+
+                letraI += 1;
+
+            } else if (palabraRandom.charAt(i) == vocalO.charAt(0)) {
+
+                letraO += 1;
+
+            } else if (palabraRandom.charAt(i) == vocalU.charAt(0)) {
+
+                letraU += 1;
+
             }
 
         }
+
         System.out.printf("""
-                La cantidad de vocales de la palabra %s son %d
-                """, palabraRandom, numeroVocales);
+            La cantidad de vocales de la palabra %s son:  
+            
+            Vocal A = %d
+            Vocal E = %d
+            Vocal I = %d
+            Vocal O = %d
+            Vocal U = %d
+            
+            """, palabraRandom, letraA, letraE, letraI, letraO, letraU);
 
     }
 }
